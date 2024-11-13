5b482c3be0772ffa9bbacc5b3684a77182468b98
firdozkhan <firdozfkhan@gmail.com>
2024-11-13 18:35:28 +0530

    change in router

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 
 )
