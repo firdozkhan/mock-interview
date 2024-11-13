68d2fb91fa4887a716415ef04f6281610d7ccccf
firdozkhan <firdozfkhan@gmail.com>
2024-11-13 13:02:22 +0530

    change h

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mock-/firdozkhan.github.io/', //can delelte
+  base: '/mock-/firdozkhan.github.io/', //can del
 })
