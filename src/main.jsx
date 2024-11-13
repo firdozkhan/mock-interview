82109ab66ead5d8f61d958ba434553b4ffe9d307
firdozkhan <firdozfkhan@gmail.com>
2024-11-13 18:18:45 +0530

    z

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 
 )
