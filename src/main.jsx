64e150f52a5393bb815922cc1ee98d3ac64d6878
firdozkhan <firdozfkhan@gmail.com>
2024-11-14 16:02:29 +0530

    change in env

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <HashRouter>
